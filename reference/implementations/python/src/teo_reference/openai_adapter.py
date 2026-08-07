69c387a445db7d238cf8093ccdfcb3e99b09e36a
SR <vessaxor@gmail.com>
2026-08-07 11:54:49 +0200

    Capture OpenAI retry timing

--- reference/implementations/python/src/teo_reference/openai_adapter.py
+++ reference/implementations/python/src/teo_reference/openai_adapter.py
@@ -10,6 +10,7 @@ from .provider_adapter import (
     ProviderExecutionRequest,
     ProviderExecutionResponse,
     ProviderFailure,
+    retry_after_seconds_from_headers,
     validate_provider_response,
 )
 from .provider_connection import (
@@ -277,6 +278,7 @@ class OpenAIResponsesAdapter:
                 code=code,
                 message=message,
             ),
+            retry_after_seconds=retry_after_seconds_from_headers(connection_response.headers),
         )
 
 
