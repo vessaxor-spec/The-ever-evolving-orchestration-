ee6e92c02fa25002ab898e587a532bce8ace9cac
SR <vessaxor@gmail.com>
2026-08-07 09:27:30 +0200

    Export provider adapter contract

--- reference/implementations/python/src/teo_reference/__init__.py
+++ reference/implementations/python/src/teo_reference/__init__.py
@@ -1,12 +1,26 @@
 """TEO Phase 5 reference orchestration engine."""
 
 from .engine import OrchestrationEngine
+from .provider_adapter import (
+    ProviderAdapter,
+    ProviderAdapterContractError,
+    ProviderExecutionRequest,
+    ProviderExecutionResponse,
+    ProviderFailure,
+    execute_provider_once,
+)
 from .schemas import DispatchRecord, FinalOutcome, TaskRequest, VerificationResult
 
 __all__ = [
     "DispatchRecord",
     "FinalOutcome",
     "OrchestrationEngine",
+    "ProviderAdapter",
+    "ProviderAdapterContractError",
+    "ProviderExecutionRequest",
+    "ProviderExecutionResponse",
+    "ProviderFailure",
     "TaskRequest",
     "VerificationResult",
+    "execute_provider_once",
 ]
