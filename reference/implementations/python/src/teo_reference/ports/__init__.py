bb91d054b85d261401c1c4f2417171fd1b47a76a
VESSAXOR <vessaxor@gmail.com>
2026-08-21 13:06:31 +0200

    refactor: add artifact integrity port package

---
+++ reference/implementations/python/src/teo_reference/ports/__init__.py
@@ -0,0 +1,5 @@
+"""Application-facing contracts for outer TEO adapters."""
+
+from .artifact import ArtifactIntegrityPort, ArtifactIntegrityPortError
+
+__all__ = ["ArtifactIntegrityPort", "ArtifactIntegrityPortError"]
